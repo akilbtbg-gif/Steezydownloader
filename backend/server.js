1694ac21d428c577a4943617922426f233d19f16
raphael <raphaelebunoluwa5@gmail.com>
2026-01-15 23:46:47 +0100

    message

--- backend/server.js
+++ backend/server.js
@@ -19,18 +19,9 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true); // allow non-browser requests (Postman, server-to-server)
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
+    origin: "*",
     methods: ["GET", "POST", "OPTIONS"],
     allowedHeaders: ["Content-Type"],
-    credentials: true, // allow credentials if needed
   })
 );
 
